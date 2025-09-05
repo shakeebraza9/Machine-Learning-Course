f1e8bf8a5684a95eb9261bacb7cc24c594c844cd
Shakeebraza <shakeebraza90@gmail.com>
2025-09-05 22:17:34 +0500

    fillna fill backword and forward and mode using to data cleaning

--- Find a null value in csv/missing_data_fill.py
+++ Find a null value in csv/missing_data_fill.py
@@ -5,3 +5,23 @@ from pandasgui import show
 dataset = pd.read_csv("loan.csv") 
 # show(dataset)
 # show(dataset.fillna(10))
+# Sirf null values ka count
+# print(dataset.isnull().sum())
+# print(dataset.info())
+
+# backword and forward filling filling
+# dataset = dataset.fillna(method="ffill" ,axis=1)
+# print(dataset)
+# perusntage = (dataset.isnull().sum()/dataset.shape[0])*100
+# print(perusntage)
+
+# Mode filling is using most use data in colume use null colume
+# dataset['Gender'].fillna(dataset['Gender'].mode()[0],inplace=True)
+# show(dataset)
+
+
+# Using for loop to collect object type Data
+for i in dataset.select_dtypes(include="object").columns:
+    dataset[i].fillna(dataset[i].mode()[0],inplace=True)
+perusntage = (dataset.isnull().sum()/dataset.shape[0])*100
+print(perusntage)
