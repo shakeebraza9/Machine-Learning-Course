c82ec36c0f740f224d4ca6e3d6d52f0d05a91f84
Shakeebraza <shakeebraza90@gmail.com>
2025-09-06 18:32:33 +0500

    lable encoding

--- Find a null value in csv/lable_encoding.py
+++ Find a null value in csv/lable_encoding.py
@@ -3,5 +3,19 @@
 
 import pandas as pd 
 from pandasgui import show
+from sklearn.preprocessing import LabelEncoder
+df = pd.DataFrame({
+    "name":["Cat","Dog","Cow","Elephant","Loin"]
+})
+
+le = LabelEncoder()
+df["en_name"] =le.fit_transform(df['name'])
+
+print(df)
 
 dataset = pd.read_csv("loan.csv")
+print(dataset["Property_Area"].unique())
+la = LabelEncoder()
+dataset['Property_Area'] = la.fit_transform(dataset['Property_Area'])
+print(dataset['Property_Area'])
+
