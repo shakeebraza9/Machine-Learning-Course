9a52918300189b3f0ac1b73482ec9857df6594b1
Shakeebraza <shakeebraza90@gmail.com>
2025-09-08 18:31:30 +0500

    outlear value find

---
+++ Find a null value in csv/outlear.py
@@ -0,0 +1,15 @@
+# outlear ya hota ha jab ek serious ma data chal raha ho or bech ma koie out value ajay us se ap ka model acha train nhi ho ga 
+# how to detack outlear discribe ma akr check krna ha mean value kiya ha or max value dono ka difference kafi zada ho to is ma outlear hone k both chnace ha 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('loan.csv')
+# print(dataset.info())
+# print(dataset.describe())
+# boxplot using to graph find the outlear value
+# sns.boxplot(x="CoapplicantIncome",data=dataset)
+# plt.show()
+
+sns.displot(dataset['ApplicantIncome'])
+plt.show()
