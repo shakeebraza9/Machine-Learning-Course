fe68a22906f8e57bf2b4591823a2ef3238d88d13
Shakeebraza <shakeebraza90@gmail.com>
2025-09-18 13:34:35 +0500

    testing

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-# Load dataset testing
+# Load dataset
 data = pd.read_csv("weight-height.csv")
 
 # ---------------- Data Cleaning ----------------
